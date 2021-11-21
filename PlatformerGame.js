af6947d96f942ca4a620fa8e9d94c112b83bdffc
stefanarctic <87850525+stefanarctic@users.noreply.github.com>
2021-11-21 21:30:42 +0200

    Added Shooting

--- PlatformerGame.js
+++ PlatformerGame.js
@@ -1,14 +1,16 @@
-// Input handling and basic player movement
 
 // Start kaboom
 kaboom()
 
 // Load assets
 loadSprite("bean", "/sprites/bean.png")
-// loadSprite("ghost", "/sprites/ghosty.png")
+loadSprite("ghost", "/sprites/ghosty.png")
 
 // Define player movement speed (pixels per second)
 const movementSpeed = 320;
+const bulletSpeed = 640;
+
+let playerDirection = RIGHT;
 
 scene("game", () => {
 
@@ -29,7 +31,18 @@ scene("game", () => {
 	    area(),
 	    solid(),
 	    color(127, 200, 255),
-		"ground"
+		"ground",
+		"obstacle"
+	]);
+
+	const enemy = add([
+		sprite("ghost"),
+		pos(width() - 150, player.pos.y),
+		scale(1),
+		area(),
+		body(),
+		EnemyHealth(),
+		"enemy"
 	]);
 
 	function Jump() {
@@ -39,18 +52,79 @@ scene("game", () => {
 	}
 
 	onUpdate(() => {
+		// Player Movement
 		if(isKeyDown("right") || isKeyDown("d")) {
 			player.move(movementSpeed, 0);
 			player.flipX(false);
+			playerDirection = RIGHT;
 		}
 		if(isKeyDown("left") || isKeyDown("a")) {
 			player.move(-movementSpeed, 0);
 			player.flipX(true);
+			playerDirection = LEFT;
 		}
 		if(isKeyDown("space") || isKeyDown("up")) {
 			Jump();
 		}
-	})
+
+		// // Shooting mechanic
+		// for(let i = 0; i < bullets.length; i++) {
+		// 	bullets[i].pos.x += playerDirection * bulletSpeed;
+		// }
+	});
+
+	function EnemyHealth() {
+		let enabled = true;
+		return {
+			id: "EnemyHealth",
+			health: 100,
+			maxhealth: 100,
+			resetHealth() {
+				this.health = this.maxhealth;
+			},
+			Die() {
+				destroy(this);
+			},
+			add() {
+				this.resetHealth();
+			},
+			GainHealth(amount) {
+				this.health += amount;
+				if(this.health > this.maxhealth) {
+					this.resetHealth();
+				}
+			},
+			TakeDamage(amount) {
+				this.health -= amount;
+				if(this.health < 0) {
+					this.health = 0;
+					this.Die();
+				}
+			}
+		}
+	}
+
+	let bullets = []
+
+	function Shoot() {
+		const bullet = add([
+			sprite("bean"),
+			pos(player.pos),
+			scale(0.5),
+			area(),
+			move(playerDirection, bulletSpeed),
+			"bullet"
+		]);
+		bullet.onCollide("enemy", (enemy) => {
+			enemy.TakeDamage(20);
+			destroy(bullet);
+		})
+		bullets.push(bullet);
+	}
+
+	onClick(() => {
+		Shoot();
+	});
 	
 });
 
